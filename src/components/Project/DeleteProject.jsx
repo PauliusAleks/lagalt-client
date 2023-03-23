ca736c74e47b1187d3729798a99ea34e95b51a25
Paulius Aleksandravicius <Paulius.Aleksandravicius@no.experis.com>
2023-03-23 17:48:41 +0100

    Feat : added deletion from the database

--- src/components/Project/DeleteProject.jsx
+++ src/components/Project/DeleteProject.jsx
@@ -3,43 +3,54 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { Form } from "react-bootstrap";
 import { useSelector, useDispatch } from "react-redux";
-// import { deleteProjectAsync } from '../../reduxParts/projectReducer';
+import { deleteProjectAsync } from '../../reduxParts/projectReducer';
+
 
 function DeleteProject() {
     const [projectToDelete, setProjectToDelete] = useState(null)
     const dispatch = useDispatch();
+    const project = useSelector((state) => state.project);
+    const projects = useSelector((state) => state.banners);
+
+    const user = useSelector((state) => state.user);
+
 
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => {
-        // setProjectToDelete({...applicationToCreate, projectId: project.id, userId:user.id})
+        setProjectToDelete({...projectToDelete, projectId: project.id, userId:user.id, admins: project.admins})
         setShow(true)
   };
 
-    const handleDeleteProject = (projectId, adminId) => {
-        // dispatch(deleteProjectAsync({projectId, adminId}));
-    }
+
+    // const handleDeleteProject = (projectId, adminId) => {
+    //     dispatch(deleteProjectAsync({projectId, adminId}));
+    // }
+
 
     const handleSubmit = () => {
-        // dispatch(deleteProjectAsync(projectToDelete))
-        //     .then((result) => {
-        //         console.log("Project was successfully deleted");
-        //         handleClose();
-        //     })
-        //     .catch((error) => {
-        //         console.log("Could not delete project:", error.message);
-        //     });
+        dispatch(deleteProjectAsync(projectToDelete))
+            .then((result) => {
+                console.log("Project was successfully deleted");
+                handleClose();
+            })
+            .catch((error) => {
+                console.log("Could not delete project:", error.message);
+            });
+        //projects.remove(projectToDelete)
       };
+
 
     return(
         <div>
-            <Button variant="primary" onClick={handleShow}>Slett prosjekt</Button>
+            <Button variant="danger" onClick={handleShow}>Slett prosjekt</Button>
+
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
                 <Modal.Title>
-                    {projectToDelete?.adminId
-                    ? `Du er i ferd med å slette ${projectToDelete.name}`
+                    {projectToDelete && projectToDelete.admins
+                    ? `Du er i ferd med å slette ${project.name}`
                     : "Bare administratorer kan slette et prosjekt!"}                
                 </Modal.Title>
                 </Modal.Header>
@@ -61,5 +72,6 @@ function DeleteProject() {
         </div>
     )
 }
+
 
 export default DeleteProject
